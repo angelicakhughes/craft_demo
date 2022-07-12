6f12ab12b9d688943b8f03f2f05da6c298ca2f41
Angelica <angelicakh@outlook.com>
2022-07-12 15:10:48 -0700

    Create miscwhileloops.py

---
+++ Misc_Data_Algo_Scripts/miscwhileloops.py
@@ -0,0 +1,40 @@
+#print i num as long as i is less than 6
+def whileloop():
+    # set i to a starting number
+    i =1
+    #do something multiple times over
+    while i <6:
+        print (i)
+        #increase i by one each time you do it over
+        i += 1
+        
+ print(whileloop())
+
+#print i num but break at 3
+def whileloop3break():
+    # set i to a starting number
+    i =1
+    #do something multiple times over
+    while i <6:
+        print (i)
+        #but if its 3 break
+        if i == 3:
+            break
+        #increase i by one each time you do it over
+        i += 1
+        
+        
+# print i num as long as i is less than 6
+def nums():
+    i =1 
+    while i < 6:
+        print (i)
+        i += 1
+    else:
+        print ("is not less than 6")
+
+print(nums) 
+
+
+
+
