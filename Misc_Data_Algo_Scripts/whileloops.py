81a64ac2ee18cd4f5d754ae40656d5fc4af0510e
Angelica <angelicakh@outlook.com>
2022-07-13 15:02:27 -0700

    Create whileloops.py

---
+++ Misc_Data_Algo_Scripts/whileloops.py
@@ -0,0 +1,17 @@
+##while loop is used as a counter
+
+def whileloop():
+    i = 1
+    while i < 10:
+        print(i)
+# +=
+        i += 1
+
+print(whileloop()) 
+
+def infinitewhileloop():
+    while True:
+        print('hello')
+        break
+
+print(infinitewhileloop())
